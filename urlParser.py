c8715714b1bd678113bd43ead62bdd71269083d4
John Williams <johnWilliams@MacBook-Pro-4.local>
2017-03-19 15:01:11 -0400

    added walsh's changes to urlParser.py

--- urlParser.py
+++ urlParser.py
@@ -1,25 +1,51 @@
 # Created by John Williams
 # This parses incoming HTTP requests for use
+
+import socket
 import urllib
 import time
+global startTime
+
+
+# this function connects to the url that is being targeted
+def connectToTarget(path, url, port):
+    theSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    address = (url, port)
+    print "connecting to target", address
+    theSock.bind(address) # TODO: believe i need to bind
+    # TODO: theSock.listen(1) don't believe i need to listen
+    connection = theSock.connect(address)
+    http = """GET """ + path + url + path + """ HTTP1.0 OK\n\r
+    Date: """ + time.time() + """
+    """ # TODO: verify this request is formatted properly
+    connection.sendall(http)
+    while True: # TODO: check loop condition
+        incoming = connection.recv(4096)
+        # where incoming is HTTP request from targeted server
+    return incoming
 
 def parser(request):
     print "request:", request
     lineList = []
     requestList = []
     timeList = []
+    hostList = []
     lineList = request.splitlines()
     print "line: ", lineList[1]
     requestList = lineList[1].split(' ')
     decoded = urllib.unquote(requestList[1])
-    trash, url = decoded.split('=')
-    timeList = lineList[2].split(' ') # the second line contains the sent at value, these must line up
-    return url, timeList[1]
+    print "decoded", decoded
+    path, target = decoded.split('?')
+    trash, host = target.split('=')
+    trash, urlWithPort = host.split('://')
+    print "host with port: ", urlWithPort
+    url, port = urlWithPort.split(':')
+    return path, url, port
 
 
 def main():
     testValue = """
-GET /geolocate?target=https%3A%2F%2Fwww.mountainproject.com/fourms/southerncalifornia HTTP2.0 OK'
+GET /geolocate?target=https%3A%2F%2Fwww.mountainproject.com/fourms/southerncalifornia:8072 HTTP2.0 OK'
 sentAt: """ + str(time.time()) + """
 Server: test
 isRealRequest = false
@@ -28,7 +54,7 @@ Date: now
 
 """
     result = parser(testValue)
-    print "Returns: ", result[1]
+    print "Returns: ", result
 
 if __name__ == '__main__':
     main()
